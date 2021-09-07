1ed23e11d561835065b98f4fd585c411355abba6
jonahvimahi <jonahvimahi@gmail.com>
2021-09-07 14:40:26 -0600

    further api functionality

--- main.js
+++ main.js
@@ -1,21 +1,37 @@
-const key = config.MY_API_KEY
-const baseURL = "http://api.weatherapi.com/v1"
+const key = config.MY_API_KEY;
+const baseURL = "http://api.weatherapi.com/v1";
 
+document.getElementById("enter").addEventListener("click", (e) => {
+    e.preventDefault()
+  let q = document.getElementById("search").value;
+  let current = "/current.json";
 
-document.getElementById('enter').addEventListener("click", () =>  {
-    let q = document.getElementById("search").value
-    let current = '/current.json'
-    
-    let options = {
-        method: 'GET',
-        url: baseURL + current,
-        params: {
-            key,
-            q,
+  let options = {
+    method: "GET",
+    url: baseURL + current,
+    params: {
+      key,
+      q,
+    },
+  };
+  axios.request(options).then((res) => {
+      console.log(res.data);
+      let weather = {
+          name: res.data.location.name,
+          region: res.data.location.region,
+          country: res.data.location.country,
+          
+          temp: res.data.current.temp_f,
+          condition:
+          res.data.current.condition.text,
+        };
+        for(item in weather){
+            let list = document.createElement('li')
+            list.textContent = weather[item]
+            document.getElementById("info").appendChild(list)
         }
-    }
-    axios.request(options).then(res => {
-        console.log(res.data)
-        
-    })
-})
+        let image = document.createElement('img')
+        image.src = res.data.current.condition.icon
+        document.getElementById('info').appendChild(image)
+  });
+});

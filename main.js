0c8bfd09a101f64d738ff48dbb67364c0f1147b4
jonahvimahi <jonahvimahi@gmail.com>
2021-09-07 13:20:42 -0600

    added api functionality

--- main.js
+++ main.js
@@ -1,8 +1,21 @@
 const key = config.MY_API_KEY
 const baseURL = "http://api.weatherapi.com/v1"
-document.getElementById('search').onclick = function () {
-    axios.get(`${baseURL}/current.json`).then
-    (function (response) {
-        const data = response.data
+
+
+document.getElementById('enter').addEventListener("click", () =>  {
+    let q = document.getElementById("search").value
+    let current = '/current.json'
+    
+    let options = {
+        method: 'GET',
+        url: baseURL + current,
+        params: {
+            key,
+            q,
+        }
+    }
+    axios.request(options).then(res => {
+        console.log(res.data)
+        
     })
-}
+})
